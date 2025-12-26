bad66d71b5feeba710a21ea75ec592de0dc92adf
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2025-12-26 12:13:45 +0500

    add full use closure code of swift

--- ClosuresInSwift.swift
+++ ClosuresInSwift.swift
@@ -1,10 +1,15 @@
-import UIKit
-
-var sumClosure = (Int , Int) -> Int
-var subClosure = (Int , Int) -> Int
+var sumClosure: (Int, Int) -> Int
 
 sumClosure = {
-    (a : Int , b : Int) -> Int in
+    (a: Int, b: Int) -> Int in
     return a + b
 }
 print("Sum is : \(sumClosure(10,20))")
+
+func calculate(numA: Int, numB: Int, operation: (Int, Int) -> Int) -> Int {
+    let result = operation(numA, numB)
+    print("Calculation is done")
+    return result
+}
+let result = calculate(numA: 1230, numB: 2560, operation: sumClosure)
+print("Result is : \(result)")
