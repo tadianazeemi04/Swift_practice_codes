3ac38841d9f8994dc0be51fcfc1bbc7e99779d7d
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2025-12-29 15:34:33 +0500

    add array0 file in array branch

---
+++ array0.swift
@@ -0,0 +1,9 @@
+let intValues: Array<Int> = [10, 20, 30, 40, 50]
+print("\nInteger Values: \(intValues)\n")
+let stringValues: [String] = ["Apple", "Banana", "Cherry"]
+print("String Values: \(stringValues)\n")
+
+var integerNumbers: Array<Int> = [Int](repeating: 20, count: 5)
+print("Integer Numbers: \(integerNumbers)\n")
+var engAlphabets: Array<Character> = ["A", "B", "C", "D", "E"]
+print("English Alphabets: \(engAlphabets)\n")
