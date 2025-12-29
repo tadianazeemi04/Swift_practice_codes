08fe77b59cfd04b9c89e67ff621fd12877abf388
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2025-12-29 16:04:21 +0500

    add more methods to be used in statments

--- array1.swift
+++ array1.swift
@@ -11,3 +11,27 @@ print("First number: \(randomNumbers.first!)")
 print("Last number: \(randomNumbers.last!)")
 print("Max number: \(randomNumbers.max()!)")
 print("Min number: \(randomNumbers.min()!)\n")
+
+if randomNumbers.contains(20) {
+    print("Array contains 20\n")
+} else {
+    print("Array does not contain 20\n")
+}
+
+if randomNumbers.count > 3 {
+    print("Array has more than 3 elements\n")
+} else {
+    print("Array has 3 or fewer elements\n")
+}
+
+if randomNumbers.isEmpty {
+    print("Array is empty\n")
+} else {
+    print("Array is not empty\n")
+}
+
+if let index = randomNumbers.firstIndex(of: 55) {
+    print("Index of 55 is \(index)\n")
+} else {
+    print("55 not found in the array\n")
+}
